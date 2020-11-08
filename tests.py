fd57311aae0ee205d9af6a8774ae13fa59aa48ec
shredsgang <goshashreeeds@gmail.com>
2020-11-09 00:39:03 +0300

    some tests

--- tests.py
+++ tests.py
@@ -7,38 +7,42 @@ auth = AuthorizationHelper(driver)
 
 
 def test_registration():
-    auth.register("Jouny", "procoder1337", "Hesoyam1337", "Hesoyam1337")
+    # Basic registration by following rules.
+    auth.register("Test", "Test1", "Hesoyam1337", "Hesoyam1337")
     assert auth.registration_seccess()
 
 def test_registration_without_name():
-    auth.register("", "Test2", "Test2", "Test2")
+    # Name is not required.
+    auth.register("", "Test2", "Hesoyam1337", "Hesoyam1337")
     assert auth.registration_seccess()
 
 def test_registration_username_check():
-    auth.register("Georgii", "Test", "Hesoyam1337", "Hesoyam1337")
+    # username must be at least 5 characters.
+    auth.register("Test", "test", "Hesoyam1337", "Hesoyam1337")
     assert not auth.registration_seccess()
 
-def test_registration_without_name():
-    auth.register("", "Test2", "Test2", "Test2")
-    assert auth.registration_seccess()
-
-def test_registration_pass_check():
-    pass
+def test_registration_password_check():
+    # password must be at least 5 characters and contain at least one number and letter.
+    auth.register("Test", "Test3", "test", "test")
+    assert not auth.registration_seccess()
 
 def test_registration_confirm_pass_check():
-    pass
+    # password must be the same as confirmation password.
+    auth.register("Test", "Test4", "Hesoyam1337", "test")
+    assert not auth.registration_seccess()
 
 def test_registration_existing_user():
-    pass
+    # trying to create account with existing user data.
+    auth.register("Test", "Test1", "Hesoyam1337", "Hesoyam1337")
+    assert not auth.registration_seccess()
 
 
 # login tests
 def test_login():
     pass
 
-def test_login_wrong_pass():
+def test_login_wrong_username():
     pass
 
-"""
-Just some local tests
-"""
+def test_login_wrong_pass():
+    pass
